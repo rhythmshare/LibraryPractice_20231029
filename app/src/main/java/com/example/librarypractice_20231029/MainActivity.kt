40dac69b9f2299866651dfd820b3f5dd92580e12
rhythmshare <randomcony@gmail.com>
2023-10-29 19:41:24 +0900

    error review - Glide error solved: The problem of not getting an image from the web

--- app/src/main/java/com/example/librarypractice_20231029/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice_20231029/MainActivity.kt
@@ -20,6 +20,8 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.imgProfile.setOnClickListener {
+//            ImageView도 클릭 이벤트 지원
+//            ViewPhotoActivity로 이동
             val myIntent = Intent(this, ViewPhotoActivity::class.java)
             startActivity(myIntent)
         }
@@ -28,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 //            전화를 바로 걸기 (번호만 눌린 화면 X, 신호까지 가도록
 //            바로 걸기 (CALL) => 사용자 요금 사용: 권한 허가 후에 가능.
 //            권한 획득 성공 여부에 따른 행동 지침을 변수에 저장(인터페이스 => 객체화 : 익명클래스 활용)
-            val pl = object  : PermissionListener{
+            val pl = object : PermissionListener{
                 override fun onPermissionGranted() {
 //                    권한 허가 되었을때 할 일 => 전화 걸기
                     val myUri = Uri.parse("tel:010-3333-5555")
@@ -39,19 +41,19 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
 //                    거절되면 => 토스트로 안내
-                    Toast.makeText(this@MainActivity, "권한 문제로 전화 연결이 안됩니다.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@MainActivity, "권한 문제로 전화 연결이 안 됩니다.", Toast.LENGTH_SHORT).show()
                 }
             }
 
 //            실제로 권한을 물어보자
             TedPermission.create()
                 .setPermissionListener(pl)
-                .setDeniedMessage("권한이 거부되었습니다.")
-                .setPermissions(Manifest.permission.CALL_PHONE)
-                .check();
-
-            Glide.with(this).load("https://en.wikipedia.org/wiki/File:David_Goggins.jpg").into()
+                .setDeniedMessage("권한을 주지 않으면 통화가 안 됩니다.")
+                .setPermissions(Manifest.permission.CALL_PHONE,Manifest.permission.INTERNET)
+                .check()
 
         }
+        //            화면이 켜지면 바로 이미지 불러내기 (웹상 이미지)
+        Glide.with(this).load("https://www.sydneyoperahouse.com/sites/default/files/styles/3x1__2560w/public/collaborodam_assets/AndrewHuberman_2560x854_syd.jpg.webp?itok=uKO3tkaW").into(binding.imgThumbnail)
     }
 }
