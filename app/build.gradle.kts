daed564e85a59273efb55737f04d84616f14a1e8
rhythmshare <randomcony@gmail.com>
2023-10-29 16:48:41 +0900

    Need to add glide part

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation ("com.github.chrisbanes:PhotoView:2.3.0")
+    implementation ("io.github.ParkSangGwon:tedpermission-normal:3.3.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/librarypractice_20231029/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice_20231029/MainActivity.kt
@@ -4,8 +4,14 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.example.librarypractice_20231029.databinding.ActivityMainBinding
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
+import android.Manifest
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
@@ -20,10 +26,32 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnCall.setOnClickListener {
 //            전화를 바로 걸기 (번호만 눌린 화면 X, 신호까지 가도록
-            val myUri = Uri.parse("tel:010-3333-5555")
+//            바로 걸기 (CALL) => 사용자 요금 사용: 권한 허가 후에 가능.
+//            권한 획득 성공 여부에 따른 행동 지침을 변수에 저장(인터페이스 => 객체화 : 익명클래스 활용)
+            val pl = object  : PermissionListener{
+                override fun onPermissionGranted() {
+//                    권한 허가 되었을때 할 일 => 전화 걸기
+                    val myUri = Uri.parse("tel:010-3333-5555")
 
-            val myIntent = Intent(Intent.ACTION_CALL, myUri)
-            startActivity(myIntent)
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+//                    거절되면 => 토스트로 안내
+                    Toast.makeText(this@MainActivity, "권한 문제로 전화 연결이 안됩니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+//            실제로 권한을 물어보자
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setDeniedMessage("권한이 거부되었습니다.")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check();
+
+            Glide.with(this).load("https://en.wikipedia.org/wiki/File:David_Goggins.jpg").into()
+
         }
     }
 }
