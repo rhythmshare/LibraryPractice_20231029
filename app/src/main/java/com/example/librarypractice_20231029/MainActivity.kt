72eb9f36e6c35915a3e2f772cf803787673c8527
rhythmshare <randomcony@gmail.com>
2023-10-29 15:11:54 +0900

    Deny phone call permission  -> Running App Shutdown

--- app/src/main/java/com/example/librarypractice_20231029/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice_20231029/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.librarypractice_20231029
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.librarypractice_20231029.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.imgProfile.setOnClickListener {
+            val myIntent = Intent(this, ViewPhotoActivity::class.java)
+            startActivity(myIntent)
+        }
+
+        binding.btnCall.setOnClickListener {
+//            전화를 바로 걸기 (번호만 눌린 화면 X, 신호까지 가도록
+            val myUri = Uri.parse("tel:010-3333-5555")
+
+            val myIntent = Intent(Intent.ACTION_CALL, myUri)
+            startActivity(myIntent)
+        }
     }
 }
